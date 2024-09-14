f77ecc763297b0e72e344b7b495636d4c6d62ff2
AmanjotSingh0908 <amanjotsingh0908@gmail.com>
2024-09-14 20:10:42 +0530

    ft. Solved kthSmallest DSA Ques.

--- kthSmallest.js
+++ kthSmallest.js
@@ -1,6 +1,39 @@
-function kthSmallest(arr, k) {
-    arr.sort((a,b) => a-b);
-    return arr[k-1]       
+// function kthSmallest(arr, k) {
+//     arr.sort((a,b) => a-b);
+//     return arr[k-1]       
+// }
+
+//Approach 2
+
+//Using Counting Sort [ref. GFG]
+function kthSmallest(Arr,k){
+    let maxElement = Arr[0];
+    for(let i=0; i<Arr.length; i++){
+        if(Arr[i] > maxElement){
+            maxElement = Arr[i]
+        }
+    }
+
+    let frequencyArray = new Array(maxElement + 1).fill(0)
+    for(let i=0; i<Arr.length; i++){
+        frequencyArray[Arr[i]]++;
+    }
+
+    let count = 0;
+    for(let i =0; i<=maxElement; i++){
+        if(frequencyArray[i] !== 0){
+            count += frequencyArray[i];
+            if(count >= k){
+                return i
+            }
+        }
+    }
+    return -1;
 }
 
-//I need another approach without sorting it 
+const arr = [4,4,8,2,1]
+const k = 3
+console.log(`${kthSmallest(arr,k)}`)
+
+// Time Complexity: O(N + max_element), where max_element is the maximum element of the array.
+// Auxiliary Space: O(max_element)
