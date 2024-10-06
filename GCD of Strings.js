d62aeb2ecab223ee5822791c101c4c3744df7440
Amanjot Singh Saini <116896075+AmanjotSingh0908@users.noreply.github.com>
2024-10-06 20:31:03 +0530

    Solved GCD of Strings.js DSA ques.

--- GCD of Strings.js
+++ GCD of Strings.js
@@ -1,1 +1,16 @@
+var gcdOfStrings = function(str1, str2) {
+    if (str1 + str2 !== str2 + str1){
+        return '';
+    }
+    function findGCD(a,b){
+        while(b !== 0){
+            let temp = b;
+            b = a%b;
+            a = temp;
+        }
+        return a;
+    }
+    let gcdLength = findGCD(str1.length, str2.length)
+    return str1.substring(0, gcdLength);
+};;
 
