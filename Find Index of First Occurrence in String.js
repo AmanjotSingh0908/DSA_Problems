c39cd7525d9d1934d57e7444125e6b810b267187
Amanjot Singh Saini <116896075+AmanjotSingh0908@users.noreply.github.com>
2024-10-08 23:52:24 +0530

    Solved Find Index of First Occurrence in String DSA Ques

--- Find Index of First Occurrence in String.js
+++ Find Index of First Occurrence in String.js
@@ -1,1 +1,16 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if(needle.length > haystack.length){
+        return -1
+    }
+    if (needle.length  == 0) {
+        return 0;
+    }
 
+    let index = haystack.indexOf(needle);
+    return index;
+};
