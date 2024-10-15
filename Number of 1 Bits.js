d98fff38ea3a73730b92eddd38e60d141ad35234
Amanjot Singh Saini <116896075+AmanjotSingh0908@users.noreply.github.com>
2024-10-15 23:50:32 +0530

    Added approach for Number of 1 Bits ques

--- Number of 1 Bits.js
+++ Number of 1 Bits.js
@@ -13,3 +13,16 @@ function hammingWeight(n) {
   
   return count;
 }
+
+//Approach 2
+function hammingWeight(n) {
+  let count = 0;
+  
+  while (n !== 0) {
+    // n & (n - 1) flips the least significant set bit to 0
+    n &= (n - 1);
+    count++;
+  }
+  
+  return count;
+}
