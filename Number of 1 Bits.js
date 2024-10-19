868871214020242f2f9b5de40928ff8dd4fc3096
AmanjotSingh0908 <amanjotsingh0908@gmail.com>
2024-10-19 15:59:03 +0530

    ft. solved   DSA Ques

--- Number of 1 Bits.js
+++ Number of 1 Bits.js
@@ -1,8 +1,6 @@
 function hammingWeight(n) {
-  // Convert number to its binary representation
   let binary = n.toString(2);
   
-  // Split the binary string into an array and count the number of '1's
   let count = 0;
   
   for (let bit of binary) {
