3b03fcf3a7845c1672a549131f73f5634a739bee
Amanjot Singh Saini <116896075+AmanjotSingh0908@users.noreply.github.com>
2025-01-10 00:07:30 +0530

    Update BestTimeToBuy&SellStocks.cpp

--- BestTimeToBuy&SellStocks.cpp
+++ BestTimeToBuy&SellStocks.cpp
@@ -26,3 +26,35 @@ public:
 
 //T.C of Memoization = O (Nx2)
 //S.C of Memeizatio = O (Nx2) + O(N) --> to remove O(N) we do tabulation
+
+#include <iostream>
+#include <vector>
+#include <algorithm>  // For max()
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<long>> dp(n + 1, vector<long>(2, 0));
+
+        dp[n][0] = dp[n][1] = 0;
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                long profit = 0;
+                if (buy) {
+                    profit = max(-prices[ind] + dp[ind + 1][0], dp[ind + 1][1]);
+                } else {
+                    profit = max(prices[ind] + dp[ind + 1][1], dp[ind + 1][0]);
+                }
+                dp[ind][buy] = profit;
+            }
+        }
+
+        return dp[0][1];
+    }
+};
+
+
