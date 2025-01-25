de46b527fe2e7c00f06b77c447ad707eceaa5f25
Amanjot Singh Saini <116896075+AmanjotSingh0908@users.noreply.github.com>
2025-01-25 19:05:37 +0530

    Update Multiply Strings.cpp

--- Multiply Strings.cpp
+++ Multiply Strings.cpp
@@ -3,6 +3,8 @@
  * @param {string} num2
  * @return {string}
  */
+//Do not try by converting strings to number directlt becoz of Precision issues over INT_SAFE_MAX
+
 var multiply = function(num1, num2) {
     if(num1 == "0" || num2 == "0") return "0";
     const result = Array(num1.length + num2.length).fill(0);
