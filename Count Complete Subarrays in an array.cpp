be24284804e85ec61f939a5be5f83dc70d544e3b
Amanjot Singh Saini <116896075+AmanjotSingh0908@users.noreply.github.com>
2025-04-24 12:46:55 +0530

    Create Count Complete Subarrays in an array.cpp

---
+++ Count Complete Subarrays in an array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+        int n = nums.size();
+
+        unordered_set<int> uniqueElements(nums.begin(), nums.end());
+        int totalDistinct = uniqueElements.size();
+
+        unordered_map<int, int> windowFreq;
+        int left = 0, ans = 0;
+        for (int right = 0; right < n; right++) {
+            windowFreq[nums[right]]++;
+
+            while (windowFreq.size() == totalDistinct) {
+                ans += (n - right);  
+                windowFreq[nums[left]]--;
+                if (windowFreq[nums[left]] == 0) {
+                    windowFreq.erase(nums[left]);
+                }
+                left++;
+            }
+        }
+
+        return ans;
+    }
+};
