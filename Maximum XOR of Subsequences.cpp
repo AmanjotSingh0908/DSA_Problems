429fd745d9da5af2eafd85176aec73db1b88d828
Amanjot Singh Saini <amanjotsingh0908@gmail.com>
2025-09-26 01:29:54 +0530

    feat : Solve max xor of subsequence LC Hard Problem
    Using gaussian elimination and vector space blog on CF
    https://codeforces.com/blog/entry/68953

---
+++ Maximum XOR of Subsequences.cpp
@@ -0,0 +1,53 @@
+class Solution
+{
+    static const int MAXLOG = 20;
+    int basis[MAXLOG];
+    int sz;
+
+    void insertVector(int mask)
+    {
+        for (int i = MAXLOG - 1; i >= 0; i--)
+        {
+            if ((mask & (1 << i)) == 0)
+                continue; // ith bit is not set in num
+
+            if (!basis[i])
+            {
+                basis[i] = mask;
+                sz++;
+                return;
+            }
+
+            mask = mask ^ basis[i];
+        }
+    }
+
+    int queryMax()
+    {
+        int res = 0;
+        for (int i = MAXLOG - 1; i >= 0; i--)
+        {
+            if (basis[i] != 0)
+            {
+                res = max(res, res ^ basis[i]);
+            }
+        }
+        return res;
+    }
+
+public:
+    int maxXorSubsequences(vector<int> &nums)
+    {
+        memset(basis, 0, sizeof(basis));
+        sz = 0;
+
+        for (int num : nums)
+        {
+            insertVector(num);
+        }
+        return queryMax();
+    }
+};
+/*
+https://codeforces.com/blog/entry/68953
+*/
